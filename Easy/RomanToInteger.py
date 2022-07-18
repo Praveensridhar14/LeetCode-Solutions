4e70a57737b3b85be71c375d0ee7c363ff3de9eb
shobi <skyshibony.17@gmail.com>
2022-07-18 17:02:25 +0530

    adding a comment for testing purpose

--- Easy/RomanToInteger.py
+++ Easy/RomanToInteger.py
@@ -2,6 +2,7 @@ class Solution:
     def romanToInt(self, s: str) -> int:
         dict_value = {"I" : 1, "V" : 5, "X" : 10, "L" : 50, "C" : 100, "D" : 500, "M" : 1000, "IV" : 4, "IX" : 9, "XL": 40, "XC" : 90, "CD":400, "CM":900}
         output_sum = 0
+        # defining the output_sum as 0
         if len(s) >= 1 and len(s) <=15:
             if "IV" in s:
                 IV_count = s.count("IV")
